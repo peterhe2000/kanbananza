a4a56b57f0154638ba30c42979aa72980c0b245c
Peter He <peter@netwealth.com.au>
2020-10-06 10:21:10 +1100

    Refactor card_create with lodash set function.

--- src/reducers/list-reducer.js
+++ src/reducers/list-reducer.js
@@ -1,19 +1,25 @@
 import { lists as defaultLists } from '../normalized-state';
+import set from 'lodash/fp/set';
+
+//set (chainOfProperties, what you want to replaced, the object you want to replace on)
 
 const listsReducer = (lists = defaultLists, action) => {
   if (action.type === 'CARD_CREATE') {
     const { cardId, listId } = action.payload;
     const entities = { ...lists.entities };
 
-    entities[listId] = {
-      ...entities[listId],
-      cards: entities[listId].cards.concat(cardId),
-    };
+    const cards = lists.entities[listId].cards.concat(cardId);
+    return set(['entities', listId, 'cards'], cards, lists);
 
-    return {
-      ...lists,
-      entities,
-    };
+    // entities[listId] = {
+    //   ...entities[listId],
+    //   cards: entities[listId].cards.concat(cardId),
+    // };
+
+    // return {
+    //   ...lists,
+    //   entities,
+    // };
   }
   return lists;
 };
