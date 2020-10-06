0caba67493e272d499bd50b21d40d7b7950b5bc0
Peter He <peter@netwealth.com.au>
2020-10-06 11:05:19 +1100

    Refactor createCard dispatch to seperate card actions and let react have way to bind Action creator with dispatch

--- src/containers/CreateCardContainer.js
+++ src/containers/CreateCardContainer.js
@@ -1,28 +1,32 @@
 import { connect } from 'react-redux';
+import { createCard } from '../actions/card-actions';
 import CreateCard from '../components/CreateCard';
 
-const defaultCardData = {
-  title: '',
-  description: '',
-  assignedTo: '',
-};
+// const defaultCardData = {
+//   title: '',
+//   description: '',
+//   assignedTo: '',
+// };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    createCard(listId, cardData) {
-      const cardId = Date.now().toString();
-      const card = {
-        id: cardId,
-        ...defaultCardData,
-        ...cardData,
-      };
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     createCard(listId, cardData) {
+//       const cardId = Date.now().toString();
+//       const card = {
+//         id: cardId,
+//         ...defaultCardData,
+//         ...cardData,
+//       };
 
-      dispatch({
-        type: 'CARD_CREATE',
-        payload: { card, listId, cardId },
-      });
-    },
-  };
-};
+//       dispatch({
+//         type: 'CARD_CREATE',
+//         payload: { card, listId, cardId },
+//       });
+//     },
+//   };
+// };
+
+// React have way to bind Action creator with dispatch
+const mapDispatchToProps = { createCard };
 
 export default connect(null, mapDispatchToProps)(CreateCard);
